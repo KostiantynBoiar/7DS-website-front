122bd2ae36ab974c5cb2bb6ffe8fd4c8baffc819
Kostiantyn Boiar <opezdalya@gmail.com>
2024-02-09 11:39:18 +0000

    Some adaptive content for snake in our mission, also added email icon with mailto: redirect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LogoIcon from './../assets/img/snake.png'
 import TelegramImg from "./../assets/img/telegram(2).png";
 import TwitterImg from "./../assets/img/X.png";
+import EmailIcon from "../assets/img/mail_icon.png"
 
 const Fotter = () => {
     return (
@@ -19,6 +20,9 @@ const Fotter = () => {
                     </li>
                     <li>
                     <a href='X.com'><img src={TwitterImg} className = 'Two'/></a>
+                    </li>
+                    <li>
+                    <a href='mailto:kostya20041234@gmail.com'><img src={EmailIcon} className = 'Two'/></a>
                     </li>
                     </ul>
                 </div>
